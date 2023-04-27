ba177192a760e7e3e1f53ae0732e73587c4cfac2
Savion Edwards <122702283+savioned@users.noreply.github.com>
2023-04-27 18:22:07 -0400

    simp

--- exercises/ex10/Simpy.py
+++ exercises/ex10/Simpy.py
@@ -15,7 +15,7 @@ class Simpy:
     # TODO: Your constructor and methods will go here.
 
     def __init__(self, values: list[float]):
-        """Sets values to a float."""
+        """Set a value to a float."""
         self.values = values
         
     def __str__(self) -> str:
@@ -23,11 +23,11 @@ class Simpy:
         return f"Simpy({self.values})"
     
     def fill(self, value: float, count: int) -> None:
-        """Allows Simpy's values to be a specific number that repeats values."""
+        """Allow Simpy's values to be a specific number that repeats values."""
         self.values = [value] * count
         
     def arange(self, start: float, stop: float, step: float = 1.0) -> None:
-        """Allows the values attribute to be a range."""
+        """Allow the values attribute to be a range."""
         assert step != 0.0, "Step argument cannot be zero."
         # self.values = [i for i in list(float(i) for i in range(int((stop-start)/step))) if i*step < stop-start] + [stop-1e-10]
         if start >= 0:
@@ -41,11 +41,11 @@ class Simpy:
         #     self.values[i] += start
             
     def sum(self) -> float:
-        """This returns the sum of all items in values."""
+        """Return the sum of all items in values."""
         return sum(self.values)
     
     def __add__(self, rhs: Union[float, Simpy]) -> Simpy:
-        """Allows for the use of addition operator."""
+        """Allow for the use of addition operator."""
         result: Simpy = Simpy([])
         if isinstance(rhs, float):
             for x in self.values:
@@ -60,7 +60,7 @@ class Simpy:
         return result
 
     def __pow__(self, rhs: Union[float, Simpy]) -> Simpy:
-        """Allows for the use of power operator."""
+        """Allow for the use of power operator."""
         result: Simpy = Simpy([])
         if isinstance(rhs, float):
             for x in self.values:
@@ -75,7 +75,7 @@ class Simpy:
         return result
 
     def __eq__(self, rhs: Union[float, Simpy]) -> list[bool]:
-        """Allows you to set things equal to one another."""
+        """Allow you to set things equal to one another."""
         result: list[bool] = []
         if isinstance(rhs, Simpy):
             assert len(self.values) == len(rhs.values)
@@ -93,7 +93,7 @@ class Simpy:
         return result
     
     def __gt__(self, rhs: Union[float, Simpy]) -> list[bool]:
-        """Tests for the greater than opertaor."""
+        """Test for the greater than opertaor."""
         result: list[bool] = []
         if isinstance(rhs, float):
             for i in self.values:
@@ -111,7 +111,7 @@ class Simpy:
         return result
     
     def __getitem__(self, rhs: Union[int, list[bool]]) -> Union[float, Simpy]:
-        """Allows for subscription notation."""
+        """Allow for subscription notation."""
         if isinstance(rhs, int):
             for i in range(len(self.values)):
                 if self.values[i] == self.values[rhs]:
